fed5a1cb22a5c28f5795feced055b39ae98a2a0a
unknown <itkomunanovisad@gmail.com>
2025-02-12 21:36:16 +0100

    onama i instalacija swiper slidera

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include 'header.php'; ?>
 
+    <!-- header -->
     <header class="homeHeader">
         <div class="hgroup">
             <h1>Dobrodošli na EKOHEROJ portal</h1>
@@ -9,5 +10,55 @@
             </div>
         </div>
     </header>
+
+    <!-- onama -->
+     <div class="onama py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 text-center">
+                    <img src="img/vector1.png" alt="">
+                    <h3>Saradnja sa nama</h3>
+                    <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <img src="img/vector2.png" alt="">
+                    <h3>Saradnja sa nama</h3>
+                    <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <img src="img/vector3.png" alt="">
+                    <h3>Saradnja sa nama</h3>
+                    <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+                </div>
+                <div class="col-md-3 text-center">
+                    <img src="img/vector4.png" alt="">
+                    <h3>Saradnja sa nama</h3>
+                    <p>Pridružite nam se u očuvanju prirode – zajedno možemo više!</p>
+                </div>
+            </div>
+        </div>
+     </div>
+
+     <!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    ...
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
+
 
 <?php include 'footer.php'; ?>
